9a410d9eff819933e3df2bc6321adb415962b3dc
hyy130202162 <2460218950@qq.com>
2021-05-14 18:03:55 +0800

    加了一个云函数

---
+++ cloudfunction/rectangel/index.js
@@ -0,0 +1,18 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+
+  return {
+    circum:(event.width+event.height)*2,
+    area:event.width*event.height
+    // event,
+    // openid: wxContext.OPENID,
+    // appid: wxContext.APPID,
+    // unionid: wxContext.UNIONID,
+  }
+}
